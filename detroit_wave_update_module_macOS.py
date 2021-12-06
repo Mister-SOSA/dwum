852071efdf2621e97092c3bdef88f1b53beb991f
Mr. SOSA <93344322+Mister-SOSA@users.noreply.github.com>
2021-12-05 19:11:45 -0600

    Update detroit_wave_update_module_macOS.py

--- detroit_wave_update_module_macOS.py
+++ detroit_wave_update_module_macOS.py
@@ -9,6 +9,7 @@ import urllib.request as urllib
 from tkinter import messagebox
 import configparser
 import sys
+import tkinter as tk
 from supabase import create_client, Client
 from subprocess import Popen, PIPE
 
@@ -16,6 +17,10 @@ url = 'https://apkhvjohfmclnxceppmh.supabase.co'
 key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzAwNzAyMywiZXhwIjoxOTUyNTgzMDIzfQ.wmgNIIak3F7szd-ZpabIMNvQXlE9qYbdc4RXdQtCn8Y'
 Client = create_client(url, key)
 real_cwd = os.getcwd()[0:os.getcwd().index('Detroit Wave Updater.app')]
+
+root = tk.Tk()
+root.overrideredirect(1)
+root.withdraw()
 
 def log_auth(hwid, attempted_key, working_directory, key_found, version_found, auth_response, ip_address, notes):
     """ Send authentication results to SQL table """
@@ -76,16 +81,16 @@ def update():
     """ Fetch newest version of kit and unzip it in the current directory """
     res = messagebox.askyesno('Detroit Wave Updater', 'Is this where you\'d like to install the sound kit?\n' + real_cwd)
     if res == False:
-        log_updates(get_hwid(), current_version(), newest_version(), 'FALSE', os.getcwd(), get_ip(), 'CANCELLED', get_ip(), 'User did not want to install to the current directory.')
-        messagebox.showinfo('Detroit Wave Updater', 'Move the updater, version.ini, and key file to the folder you\'d like to install the kit. Then run the updater again.')
+        log_updates(get_hwid(), current_version(), newest_version(), 'FALSE', os.getcwd(), get_ip(), 'CANCELLED', 'User did not want to install to the current directory.')
+        messagebox.showinfo('Detroit Wave Updater', 'Move the updater to the folder you\'d like to install the kit. Then run the updater again.')
         quit()
     else:
-        messagebox.showinfo('Detroit Wave Updater', 'The newest version of Detroit Wave will be now be installed here.')
+        messagebox.showinfo('Detroit Wave Updater', 'The newest version of Detroit Wave will be installed here when you click OK.\nPlease be patient as the update is downloaded.\nThe program may appear unresponsive while downloading.')
     if (os.path.isdir(real_cwd + 'Alex Kure - Sounds of Detroit III')):
         try:
             shutil.rmtree(real_cwd + 'Alex Kure - Sounds of Detroit III/')
         except:
-            log_updates(get_hwid(), current_version(), newest_version(), 'TRUE', os.getcwd(), get_ip(),'FAILED', get_ip(), 'Update failed due to a permissions issue.')
+            log_updates(get_hwid(), current_version(), newest_version(), 'TRUE', os.getcwd(), get_ip(),'FAILED', 'Update failed due to a permissions issue.')
             messagebox.showerror(
                 'Permissions Issue', 'Update failed due to a permissions issue. Try running the updater as administrator.')
             quit()
@@ -117,13 +122,13 @@ def main():
     if not (os.path.exists('key.dll')):
         log_auth(get_hwid(), 'NOT FOUND', os.getcwd(), 'FALSE', 'N/A', 'N/A', get_ip(), 'key.dll was not found in the current directory.')
         messagebox.showerror(
-            'Missing Key', 'Your "key" file was not found. Make sure it is in the same folder as the updater.')
+            'Missing Key', 'Your "key" file was not found.')
         quit()
 
     if not (os.path.exists('version.ini')):
         log_auth(get_hwid(), config['REGISTRATION']['hwid'], os.getcwd(), 'TRUE', 'FALSE', 'N/A', get_ip(), 'version.ini was not found in the current directory.')
         messagebox.showerror(
-            'Missing version.ini', 'Your version.ini file was not found in this folder. Make sure it is in the same folder as the updater')
+            'Missing version.ini', 'Your version.ini file was not found.')
         quit()
 
     """ Check HWID with provided key file. If matched, proceed. If not, quit with error """
@@ -136,11 +141,11 @@ def main():
         except:
             log_auth(get_hwid(), config['REGISTRATION']['hwid'], os.getcwd(), 'TRUE', 'TRUE', 'FAILED', get_ip(), 'First time registration failed.')
             messagebox.showinfo(
-                'Registration Failed!', 'First time setup failed for some unknown reason.\nTry running as administrator.')
+                'Registration Failed!', 'First time setup failed for some unknown reason.')
 
     if (config['REGISTRATION']['HWID'] != get_hwid()):
         log_auth(get_hwid(), config['REGISTRATION']['hwid'], os.getcwd(), 'TRUE', 'TRUE', 'FAILED', get_ip(), 'Piracy Protection Triggered.')
-        messagebox.showerror('Validation Failed!', 'Your device is not registered with this soundkit. Make sure you have purchased the soundkit and placed the provided key file in the same folder as this updater.\n\nFor assistance, DM me on Instagram: @AlexKure')
+        messagebox.showerror('Validation Failed!', 'Your device is not registered with this soundkit. Make sure you have purchased the soundkit and are using your own copy of the updater.\n\nFor assistance, DM me on Instagram: @AlexKure')
         quit()
     else:
         log_auth(get_hwid(), config['REGISTRATION']['hwid'], os.getcwd(), 'TRUE', 'TRUE', 'SUCCESS', get_ip(), 'Successful Login.')
