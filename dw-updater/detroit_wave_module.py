20e2221a8417c0f432362c636e137a0e383ddad2
Mr. SOSA <93344322+Mister-SOSA@users.noreply.github.com>
2022-02-21 16:10:50 -0600

    Update detroit_wave_module.py

--- dw-updater/detroit_wave_module.py
+++ dw-updater/detroit_wave_module.py
@@ -12,11 +12,14 @@ import sys
 
 def get_hwid():
     """ Fetch this machine's HWID for authentication """
-    cmd = 'wmic csproduct get uuid'
-    uuid = str(subprocess.check_output(cmd))
-    pos1 = uuid.find("\\n") + 2
-    uuid = uuid[pos1:-15]
-    return uuid
+    try:
+        cmd = 'wmic csproduct get uuid'
+        uuid = str(subprocess.check_output(cmd))
+        pos1 = uuid.find("\\n") + 2
+        uuid = uuid[pos1:-15]
+        return uuid
+    except:
+        return "FAILED"
 
 
 
